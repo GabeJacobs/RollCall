601b41b8f55b24e3c405563723b5dca28dfdf8c3
GabeJacobs <GabeJacobs824@gmail.com>
2014-07-04 16:18:32 -0400

    initial commit

---
+++ RollCall/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RollCall
+//
+//  Created by Gabe Jacobs on 7/4/14.
+//  Copyright (c) 2014 Gabe Jacobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
