d474ca01fd0b4c8ea903b33af66659cc7d91571a
Amadou Crookes <ascrookes@gmail.com>
2014-07-10 00:37:01 -0400

    put in stubs for network manager

--- RollCall/Managers/RCNetworkManager.h
+++ RollCall/Managers/RCNetworkManager.h
@@ -8,6 +8,13 @@
 
 #import <Foundation/Foundation.h>
 
+typedef void (^rcImageSuccessBlock) (UIImage* image);
+typedef void (^rcImageFailureBlock) (NSError* error);
+
 @interface RCNetworkManager : NSObject
+
++ (void)getImageAtURL:(NSString*)url
+              success:(rcImageSuccessBlock)completion
+              failure:(rcImageFailureBlock)failure;
 
 @end
--- RollCall/Managers/RCNetworkManager.m
+++ RollCall/Managers/RCNetworkManager.m
@@ -8,6 +8,35 @@
 
 #import "RCNetworkManager.h"
 
+@interface RCNetworkManager ()
+@property (nonatomic) NSCache *imageCache;
+@end
+
 @implementation RCNetworkManager
+
++ (RCNetworkManager*)sharedManager {
+    static RCNetworkManager* manager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        manager = [[RCNetworkManager alloc] init];
+    });
+    return manager;
+}
+
+// TODO(amadou): Implement this.
++ (void)getImageAtURL:(NSString*)url
+              success:(rcImageSuccessBlock)success
+              failure:(rcImageFailureBlock)failure {
+    success([UIImage imageNamed:@"Phone.png"]);
+}
+
+#pragma mark - Setters and Getters
+
+- (NSCache*)imageCache {
+    if (_imageCache) {
+        _imageCache = [[NSCache alloc] init];
+    }
+    return _imageCache;
+}
 
 @end
